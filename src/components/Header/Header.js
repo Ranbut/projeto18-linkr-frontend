107c8ba1dc2f341ab3bc3b71f6295816a1d7de5e
Hugo Junior <hjunioreng@gmail.com>
2023-03-09 06:13:47 -0300

    feat header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,55 @@
+import { useState } from "react";
+import { icons } from "react-icons";
 import { HeaderBody } from "./style";
-
+import {AiOutlineSearch} from "react-icons/ai"
+import {RiArrowDownSLine} from "react-icons/ri"
 export default function Header(){
+
+    const [form,setForm] = useState({
+        username:'',
+    })
+
+const [input, setInput] = useState('false')
+
+    function handleFilter(e){
+       e.preventDefault()
+
+    }
+    
+
+     function handleForm(e){
+      setForm({
+        ...form,
+        [e.target.name] : e.target.value
+    })    
+    }
+
+    
     return (
         <HeaderBody>
-            <h4>linkr</h4>
+    <span>linkr</span>
+
+<section>
+    { input === true ?
+    <></>:
+
+        <form onSubmit={handleFilter}>
+        <input placeholder="Search for people"
+         name="username" 
+         onChange={handleForm} 
+         value={form.username} 
+         type="text" 
+         required>
+         </input>
+        <button type="submit"><AiOutlineSearch/></button>
+    </form>
+        }
+    </section>
+
+    <span>
+        <p><RiArrowDownSLine/></p>
+<img src="#" alt="foto"/> 
+    </span>
         </HeaderBody>
         );
 }
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -4,11 +4,18 @@ export const HeaderBody = styled.header `
 width: 100%;
 height: 72px;
 background: #151515;
+display:flex;
+justify-content: space-between;
+align-items: center;
 
-    h4{
+section{
+    min-height:45px;
+    background-color:#E7E7E7;
+    border-radius: 5px;
+ 
+}
+    span{
         padding: 10px 28px;
-        width: 108px;
-        height: 54px;
         font-family: 'Passion One';
         font-style: normal;
         font-weight: 700;
@@ -16,5 +23,25 @@ background: #151515;
         line-height: 54px;
         letter-spacing: 0.05em;
         color: #FFFFFF;
+    }
+    input{
+        width: 563px;
+        height:45px;
+        border-radius: 5px;
+        ::placeholder{
+            font-family: 'Lato';
+            font-size: 19px;
+            font-weight: 400;
+            line-height: 23px;
+            letter-spacing: 0em;
+            text-align: left;
+            color:#C6C6C6;
+            padding-left:17px;
+                   
+        }
+        button{
+            background-color:transparent;
+            border:none;
+        }
     }
 `;
